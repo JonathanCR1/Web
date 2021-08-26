80360bc32de9328c810709d4f859effc865afa06
jonathanCaamano <JONATHANCRI@ext.inditex.com>
2021-08-26 13:51:12 +0200

    add blog

---
+++ blog/pages/[slug].js
@@ -0,0 +1,30 @@
+import { getFiles, getFilesBySlug } from "../lib/mdx"
+import { MDXRemote } from 'next-mdx-remote';
+import MDXComponents from "../../components/MDXComponents";
+
+export default function Post({ source, frontmatter}) {
+    return <MDXRemote {...source} />;
+}
+
+export async function getStaticProps( { params} ){
+
+    const {source, frontmatter} = await getFilesBySlug(params.slug);
+    return{
+        props: {source, frontmatter},
+    };
+}
+
+export async function getStaticPaths() {
+
+    const post = await getFiles();
+    const paths = posts.map((post) =>({
+        params: {
+            slug: post.replace(/\.mdx/,""),
+        
+        },
+    }));
+    return {
+        paths,
+        fallback: false}
+
+}
